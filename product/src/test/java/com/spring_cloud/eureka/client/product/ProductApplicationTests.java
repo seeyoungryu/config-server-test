c5b12b7af52c80c234a919daa738e90f0c9b9179
seeyoungryu <seayeux@gmail.com>
2024-08-06 00:22:37 +0900

    init commit

---
+++ product/src/test/java/com/spring_cloud/eureka/client/product/ProductApplicationTests.java
@@ -0,0 +1,15 @@
+package com.spring_cloud.eureka.client.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
