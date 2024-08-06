8cc3fb5ca097a0b76e23d59eb4c9de2b11daf24b
seeyoungryu <seayeux@gmail.com>
2024-08-06 11:02:26 +0900

    config server test (actuator/refresh)

--- product/src/main/java/com/spring_cloud/eureka/client/product/ProductApplication.java
+++ product/src/main/java/com/spring_cloud/eureka/client/product/ProductApplication.java
@@ -2,9 +2,9 @@ package com.spring_cloud.eureka.client.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+
+
 @SpringBootApplication
 public class ProductApplication {
 
